b891247a5eaa075c59c114f84c54598904a8c0bf
Dat Ngo <dat.ngo@aifora.com>
2022-02-17 16:23:17 +0700

    Remove hello function

--- handler.js
+++ handler.js
@@ -3,23 +3,6 @@ const AWS = require('aws-sdk')
 const s3 = new AWS.S3()
 const csv = require('@fast-csv/parse');
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
-
 module.exports.readS3File = async (event) => {
   const Key = event.Records[0].s3.object.key;
 

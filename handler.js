2cb327e9cdfff3acce791a31baa54e2b0c2c1cf9
Dat Ngo <dat.ngo@aifora.com>
2022-02-17 15:51:48 +0700

    Parse convent from CSV with stream

--- handler.js
+++ handler.js
@@ -23,20 +23,32 @@ module.exports.hello = async (event) => {
 module.exports.readS3File = async (event) => {
   const Key = event.Records[0].s3.object.key;
 
-    // Read the object from S3
-    const data = await s3.getObject({
-        Bucket: event.Records[0].s3.bucket.name,
-        Key
-    }).promise();
+    const s3BucketName = event.Records[0].s3.bucket.name;
+    const s3ObjectKey = event.Records[0].s3.object.key;
+    const params = {
+      Bucket: s3BucketName,
+      Key: s3ObjectKey,
+    };
+    const csvFile = s3.getObject(params).createReadStream();
 
-    // const s3Object = JSON.parse(data.Body) -- disabled due to no more needed
-    // console.log("s3Object", s3Object); -- disabled due to no more needed
-    // -- begin inspecting
-
-    event.Records.forEach((item) => {
-      console.log(item);
+    let csvParser = new Promise((resolve, reject) => {
+      const parser = csv
+        .parseStream(csvFile, { headers: true })
+        .on("data", function (readdata) {
+          console.log('Data parsed from CSV: ', readdata);
+        })
+        .on("end", function () {
+          resolve("CSV parsing finished");
+        })
+        .on("error", function () {
+          reject("CSV parsing failed");
+        });
     });
-    // -- end inspecting
 
+    try {
+      await csvParser;
+    } catch (error) {
+      console.log("Get Error: ", error);
+    }
     return;
 };

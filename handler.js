e7cbdf9d372471b046e2b42ba0be5ada2f75ef22
datngoNFQ <57701098+datngoNFQ@users.noreply.github.com>
2022-02-19 09:25:29 +0700

    Feature/post voucher code (#5)
    * Impl API Post for Customer to submit voucher
    
    * Validate param
    
    * Query to validate voucher code , and status of use
    
    * Set voucher to be used, sql with transaction and commit and rollback
    
    * clean up code, and handle rollback with error message
    
    * Capture the 1st query result to check for used voucher
    
    * Handle error when voucher has been used
    
    Co-authored-by: Dat Ngo <dat.ngo@aifora.com>

--- handler.js
+++ handler.js
@@ -24,6 +24,63 @@ const insertToDB = async (voucher) => {
   return insertresults;
 }
 
+module.exports.applyvoucher = async (event) => {
+  const data = JSON.parse(event.body);
+  let errResponse = {
+    statusCode: 400,
+    body: ''
+  }
+
+  const {voucher, customer_id} = data;
+  if( !voucher ) {
+    return {...errResponse, body: JSON.stringify({"message": "Param voucher is invalid"})}
+  }
+  if( !customer_id ) {
+    return {...errResponse, body: JSON.stringify({"message": "Param customer_id is invalid"})}
+  }
+
+  console.log('Begin processing');
+  await mysql.connect();
+  const connection = mysql.getClient();
+  const currentMoment =  moment(new Date()).format("YYYY-MM-DD HH:mm:ss");
+
+  let results = await mysql.transaction()
+  .query('SELECT * FROM vouchers WHERE voucher_code = ? and used = ? LIMIT 1 FOR UPDATE', [voucher, false])
+  // .query('UPDATE vouchers SET customer_id=?, used = ?, updated_at=? WHERE voucher_code = ?', [customer_id, true, currentMoment, voucher])
+  .query((r) => {
+    console.log("r == ",r);
+    if (r.length === 0) {
+      errResponse = {
+        statusCode: 502,
+        body: JSON.stringify({"message": "Voucher is not valid or had been used!"})
+      };
+      return null;
+    }
+    else {
+      return ['UPDATE vouchers SET customer_id=?, used = ?, updated_at=? WHERE voucher_code = ?', [customer_id, true, currentMoment, voucher]];
+    }
+  })
+  .rollback(e => {
+    console.log('DB record update error: ', e);
+    errResponse = {
+      statusCode: 502,
+      body: JSON.stringify({"message": "Param voucher is invalid"})}
+  })
+  .commit();
+
+  await mysql.end();
+  console.log('End processing');
+  console.log('errResponse == ', errResponse);
+  if (errResponse.statusCode === 502) {
+    return errResponse;
+  };
+
+  const response = {
+    statusCode: 201,
+    body: JSON.stringify(data)
+  };
+  return response;
+}
 module.exports.readS3File = async (event) => {
   const Key = event.Records[0].s3.object.key;
 

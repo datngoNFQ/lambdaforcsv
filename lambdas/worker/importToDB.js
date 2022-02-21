297561c8062ff98023edd96a08d4f10fe6b3d661
Dat Ngo <dat.ngo@aifora.com>
2022-02-21 17:08:36 +0700

    prepare code to import csv to db

--- lambdas/worker/importToDB.js
+++ lambdas/worker/importToDB.js
@@ -1,10 +1,35 @@
 'use strict';
+const AWS = require('aws-sdk')
+const s3 = new AWS.S3()
+const csv = require('@fast-csv/parse');
+const mysql = require('serverless-mysql')();
+const moment = require('moment');
+const multipart = require('aws-lambda-multipart-parser');
 
+mysql.config({
+  host     : process.env.ENDPOINT,
+  database     : process.env.DATABASE,
+  user         : process.env.DB_USER,
+  password     : process.env.DB_PASSWORD,
+});
+
+const insertToDB = async (voucher) => {
+  const results = await mysql.query('select * from vouchers;');
+  const currentMoment =  moment(new Date()).format("YYYY-MM-DD HH:mm:ss");
+
+  let insertresults = await mysql.transaction()
+  .query('INSERT INTO vouchers (voucher_code,created_at) VALUES(?,?)', [voucher,currentMoment])
+  .commit();
+  return insertresults;
+}
+
+/**
+ * This lambda read CSV file from S3
+ * Then import to AWS RDS (MySQL)
+ * @param {*} event: which is triggered by S3's event
+ * @returns
+ */
 exports.handler = async (event) => {
-  const response = {
-    statusCode: 200,
-    body: 'Welcome to Read CSV worker'
-  }
   console.log('Worker: Read CSV and import to database');
-  return response;
+  return;
 };

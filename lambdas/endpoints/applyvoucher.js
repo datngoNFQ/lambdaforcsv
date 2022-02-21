cec73c5e3f444bd94b764f5db416b9a8c14fc42e
Dat Ngo <dat.ngo@aifora.com>
2022-02-21 15:00:25 +0700

    Clean up file serverless yml

--- lambdas/endpoints/applyvoucher.js
+++ lambdas/endpoints/applyvoucher.js
@@ -3,7 +3,7 @@
 exports.handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: 'Welcome to APPLY a vouncher code'
+    body: 'Welcome to APPLY a vouncher code from customer'
   }
   console.log('API Endpoint: APPLY a voucher code');
   return response;

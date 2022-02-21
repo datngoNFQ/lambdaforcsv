8088a72c5f90b7d8e98c49c067e23dc1ddc65203
Dat Ngo <dat.ngo@aifora.com>
2022-02-21 16:16:10 +0700

    Add more needed libs for handler uploadcsv

--- lambdas/endpoints/uploadCsv.js
+++ lambdas/endpoints/uploadCsv.js
@@ -1,5 +1,9 @@
 'use strict';
 const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+const csv = require('@fast-csv/parse');
+const moment = require('moment');
+const multipart = require('aws-lambda-multipart-parser');
 
 exports.handler = async (event) => {
   const response = {

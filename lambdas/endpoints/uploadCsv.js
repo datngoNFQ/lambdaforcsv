3bf5fc48d8f6df4548adaa9856d0f84fb137bd6d
Dat Ngo <dat.ngo@aifora.com>
2022-02-21 16:47:11 +0700

    Finish upload file to csv, with uuid at nameing, with Env at Aws Lambda config

--- lambdas/endpoints/uploadCsv.js
+++ lambdas/endpoints/uploadCsv.js
@@ -4,20 +4,16 @@ const s3 = new AWS.S3();
 const csv = require('@fast-csv/parse');
 const moment = require('moment');
 const multipart = require('aws-lambda-multipart-parser');
+const { v4: uuidv4 } = require('uuid');
 
 exports.handler = async (event) => {
   console.log('BEGIN file upload handling');
-  console.log('event == ', event);
-  console.log('event body == ', event.body);
-  console.log('event typeof body == ', typeof event.body);
-  // console.log('event.body.filename == ', event.body.filename); // undefined
-  // const data = JSON.parse(event.body);
-  // console.log('data == ', data);
   try {
     const spotText = true; // all text files are present in text for after parsing
     const deta = multipart.parse(event, spotText);
-    const s3BucketName = "import-csv-with-lambda-rev1";
-    const s3ObjectKey = "myvouchers.csv";
+    const s3BucketName = process.env.S3BUCKET;
+    const uuid = uuidv4();
+    const s3ObjectKey = `${process.env.FILENAME}_${uuid}`;
 
     const params = {
       Bucket: s3BucketName,
@@ -29,7 +25,6 @@ exports.handler = async (event) => {
     if (!newData) {
       throw Error('there was an error writing the file');
     }
-    // END s3 write
 
   } catch(e) {
     console.log(e);

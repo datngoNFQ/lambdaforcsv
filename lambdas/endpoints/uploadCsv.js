7ba20a867b3b3849e47d3d6296b532306a55b9ac
Dat Ngo <dat.ngo@aifora.com>
2022-02-21 16:36:40 +0700

    Save uploaded file

--- lambdas/endpoints/uploadCsv.js
+++ lambdas/endpoints/uploadCsv.js
@@ -6,22 +6,17 @@ const moment = require('moment');
 const multipart = require('aws-lambda-multipart-parser');
 
 exports.handler = async (event) => {
-  console.log('BEGIN function procssing == ');
+  console.log('BEGIN file upload handling');
   console.log('event == ', event);
   console.log('event body == ', event.body);
   console.log('event typeof body == ', typeof event.body);
   // console.log('event.body.filename == ', event.body.filename); // undefined
   // const data = JSON.parse(event.body);
   // console.log('data == ', data);
-  /*
   try {
-    // const data = JSON.parse(event.body);
     const spotText = true; // all text files are present in text for after parsing
     const deta = multipart.parse(event, spotText);
-    console.log("deta == ");
-    console.log(deta);
-    // BEGIN s3 write
-    const s3BucketName = "import-csv-with-lambda";
+    const s3BucketName = "import-csv-with-lambda-rev1";
     const s3ObjectKey = "myvouchers.csv";
 
     const params = {
@@ -30,25 +25,20 @@ exports.handler = async (event) => {
       Key: s3ObjectKey,
     };
 
-    // const newCSVData = await s3.write(data, fileName, bucket) // TODO: impl this functions
-    // .catch(err => {
-    //   console.log('error in s3 write', err);
-    //   return null;
-    // });
     const newData = await s3.putObject(params).promise();
     if (!newData) {
       throw Error('there was an error writing the file');
     }
     // END s3 write
 
-
   } catch(e) {
     console.log(e);
-  } */
+  }
 
   const response = {
     statusCode: 201,
     body: JSON.stringify({"message": "File is uploaded!"})
   };
+  console.log('END file upload handling');
   return response;
 };

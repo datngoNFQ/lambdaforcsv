a5174cba6a976bb7c92e73ec4ad2ae27edefe67b
datngoNFQ <57701098+datngoNFQ@users.noreply.github.com>
2022-02-21 16:57:28 +0700

    Feature/move functions to lambda folder sub making webpack to work (#7)
    * Config webpack to notload aws-sdk; begin to elaborate uploadcsv function
    
    * Add more needed libs for handler uploadcsv
    
    * Prepare content for uploading csv
    
    * Save uploaded file
    
    * Finish upload file to csv, with uuid at nameing, with Env at Aws Lambda config
    
    Co-authored-by: Dat Ngo <dat.ngo@aifora.com>

--- lambdas/endpoints/uploadCsv.js
+++ lambdas/endpoints/uploadCsv.js
@@ -1,10 +1,39 @@
 'use strict';
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+const csv = require('@fast-csv/parse');
+const moment = require('moment');
+const multipart = require('aws-lambda-multipart-parser');
+const { v4: uuidv4 } = require('uuid');
 
 exports.handler = async (event) => {
-  const response = {
-    statusCode: 200,
-    body: 'Welcome to upload csv API Endpoint'
+  console.log('BEGIN file upload handling');
+  try {
+    const spotText = true; // all text files are present in text for after parsing
+    const deta = multipart.parse(event, spotText);
+    const s3BucketName = process.env.S3BUCKET;
+    const uuid = uuidv4();
+    const s3ObjectKey = `${process.env.FILENAME}_${uuid}`;
+
+    const params = {
+      Bucket: s3BucketName,
+      Body: deta.file.content,
+      Key: s3ObjectKey,
+    };
+
+    const newData = await s3.putObject(params).promise();
+    if (!newData) {
+      throw Error('there was an error writing the file');
+    }
+
+  } catch(e) {
+    console.log(e);
   }
-  console.log('API Endpoint: Upload csv file');
+
+  const response = {
+    statusCode: 201,
+    body: JSON.stringify({"message": "File is uploaded!"})
+  };
+  console.log('END file upload handling');
   return response;
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,44 @@
 module.exports = {
   target: 'node',
+  externals: {'aws-sdk': 'aws-sdk'},
   mode: 'none'
 }
+
+
+
+// webpack.config.js
+// const path = require('path');
+// const slsw = require('serverless-webpack');
+
+// module.exports = {
+//   entry: slsw.lib.entries,
+//   target: 'node',
+//   mode: 'none',
+//   stats: 'minimal',
+//   devtool: 'nosources-source-map',
+//   devtool: 'source-map',
+//   // Since 'aws-sdk' is not compatible with webpack,
+//   // we exclude all node dependencies
+//   externals: {'aws-sdk': 'aws-sdk'},
+//   performance: {
+//     hints: false,
+//   },
+//   resolve: {
+//     extensions: ['.js', '.jsx', '.json'],
+//   },
+//   output: {
+//     libraryTarget: 'commonjs2',
+//     path: path.join(__dirname, '.webpack'),
+//     filename: '[name].js',
+//     sourceMapFilename: '[file].map',
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: '/\.js$/',
+//         include: __dirname,
+//         exclude: '/node_modules/'
+//       }
+//     ]
+//   }
+// };

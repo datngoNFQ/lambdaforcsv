fed954f5680a6ee0b566345c068a7e8f808b89c0
Dat Ngo <dat.ngo@aifora.com>
2022-02-21 17:28:18 +0700

    Save data to database

--- lambdas/worker/importToDB.js
+++ lambdas/worker/importToDB.js
@@ -7,7 +7,7 @@ const moment = require('moment');
 const multipart = require('aws-lambda-multipart-parser');
 
 mysql.config({
-  host     : process.env.ENDPOINT,
+  host         : process.env.ENDPOINT,
   database     : process.env.DATABASE,
   user         : process.env.DB_USER,
   password     : process.env.DB_PASSWORD,
@@ -31,5 +31,41 @@ const insertToDB = async (voucher) => {
  */
 exports.handler = async (event) => {
   console.log('Worker: Read CSV and import to database');
-  return;
+  const Key = event.Records[0].s3.object.key;
+
+    const s3BucketName = event.Records[0].s3.bucket.name;
+    const s3ObjectKey = event.Records[0].s3.object.key;
+    console.log("S3 Bucket: " , s3BucketName);
+    const params = {
+      Bucket: s3BucketName,
+      Key: s3ObjectKey,
+    };
+    const csvFile = s3.getObject(params).createReadStream();
+
+    let csvParser = new Promise((resolve, reject) => {
+      const parser = csv
+        .parseStream(csvFile, { headers: true })
+        .on("data", function (readdata) {
+          const { VoucherCode } = readdata;
+          insertToDB(VoucherCode);
+        })
+        .on("end", function () {
+          resolve("CSV parsing finished");
+        })
+        .on("error", function () {
+          reject("CSV parsing failed");
+        });
+    });
+
+    try {
+      console.log('BEGIN - dumping data to database');
+      await mysql.connect();
+      await csvParser;
+      await mysql.end();
+      console.log('END - dumping data to database');
+    } catch (error) {
+      console.log("Database import error: ", error);
+    }
+
+    return;
 };

9ae472e373f43e9acc0d95fa8b7bbe31134733a9
Dat Ngo <dat.ngo@aifora.com>
2022-02-22 10:08:47 +0700

    prepare library to update to db

--- lambdas/endpoints/applyvoucher.js
+++ lambdas/endpoints/applyvoucher.js
@@ -1,6 +1,9 @@
 'use strict';
+const mysql = require('serverless-mysql')();
+const moment = require('moment');
 
 exports.handler = async (event) => {
+  console.log('HELLO WORLD');
   const response = {
     statusCode: 200,
     body: 'Welcome to APPLY a vouncher code from customer'
